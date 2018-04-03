7b2db54c8c0562ce41c59b5eef62ce078670703e
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2018-04-02 23:08:40 -0400

    test

--- app.js
+++ app.js
@@ -53,14 +53,15 @@ function overviewIntent(app) {
 
 
 function optionIntent(app) {
-    console.log('helo', app.getSelectedOption())
+    console.log('selected', app.getSelectedOption())
     if (app.getSelectedOption() === 'Fund Data') {
-        app.tell(`The fund trade symbol is QQQ. With an  intraday NAV (IIV) is QXV.
+        app.ask(`The fund trade symbol is QQQ. With an  intraday NAV (IIV) is QXV.
         The QQQ CUSIP is 73935A104 and it is listed on the NASDAQ exchange.
         As of December, 31st 2017 there are 106 securities in the ETF, with a 30-Day SEC Yield of .86%, 
         a 30-Day SEC Unsubsidized Yield and a Total Expense Ratio of .20%`);
     } else if (app.getSelectedOption() === 'Fund Description') {
-        app.tell(`webhook`);
+        app.ask(`PowerShares QQQ is an exchange-traded fund, or ETF, based on the Nasdaq 100 Index.  The Fund will, under most circumstances, consist of all the stocks in the Index.  The Index includes 100 of the largest domestic and international nonfinancial companies listed on the Nasdaq stock market based on market capitalization.  
+        The Fund and the Index are rebalanced quarterly and reconstituted annually`);
     }
 }
 
